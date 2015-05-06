5d3a75b4e6834d881d2b0215ae27dc957bee3d42
BuildTools <christian@decode.org.nz>
2015-05-07 06:25:42 +1200

    Add extension code

--- generators/extension/templates/Extension.php
+++ generators/extension/templates/Extension.php
@@ -8,11 +8,15 @@ use Symfony\Component\ClassLoader\Psr4ClassLoader;
 
 class Extension extends BaseExtension {
     protected $loader;
+    protected $twigHelper;
 
     public function initialize() {
         $this->loader = new Psr4ClassLoader();
         $this->loader->addPrefix('Bolt\\Extension\\<%= owner %>\\<%= shortName %>', __DIR__ . '/src');
         $this->loader->register();
+
+        $this->twigHelper = new TwigHelper();
+        $this->twigHelper->addTwigFunctions();
     }
 
     public function getName() {
--- generators/extension/templates/src/TwigHelper.php
+++ generators/extension/templates/src/TwigHelper.php
@@ -2,4 +2,88 @@
 
 namespace Bolt\Extension\<%= owner %>\<%= shortName %>;
 
-// TODO: Implement this
+class TwigHelper {
+
+    protected $manifest;
+    protected $app;
+    protected $extension;
+
+    public function __construct($app, $extension) {
+        $this->app = $app;
+        $this->extension = $extension;
+    }
+
+    /**
+     * Add all of the Twig functions and filters
+     */
+    public function addTwigFunctions() {
+        $this->initializeTwig();
+        $this->twigExtension->addTwigFilter(new \Twig_SimpleFilter('preg_replace', array($this, '_preg_replace')));
+        $this->twigExtension->addTwigFunction(new \Twig_SimpleFunction('asset', array($this, 'gulpRevBust')));
+    }
+
+    /**
+	 * Perform a regular expression search and replace.
+	 *
+	 * @param string $subject
+	 * @param string $pattern
+	 * @param string $replacement
+	 * @param int $limit
+	 * @return string
+	 */
+	public function _preg_replace($subject, $pattern, $replacement = '', $limit = -1)
+	{
+		if (!isset($subject)) {
+			return null;
+		}
+		else {
+			return preg_replace($pattern, $replacement, $subject, $limit);
+		}
+	}
+
+    /**
+     * @param string $accessorName
+     * @param string $prefix - An optional string to
+     * @return string
+     */
+    public function gulpRevBust($accessorName = '', $prefix = null) {
+        if ($prefix === null) {
+            $prefix = $this->app['resources']->getUrl('theme');
+        }
+
+        if (trim($accessorName) == '') {
+            return '';
+        }
+
+        $fileName = $this->getManifestFileName();
+        if (!file_exists($fileName)) {
+            return $prefix . 'assets/' . $accessorName;
+        }
+
+        if (!isset($this->manifest)) {
+            $this->manifest = json_decode(file_get_contents($fileName), true);
+        }
+
+        $options = $this->manifest;
+
+        return $prefix . 'assets/' . (isset($options[$accessorName]) ? $options[$accessorName] : $accessorName);
+    }
+
+    /**
+     * @return string
+     */
+    protected function getDefaultFilename() {
+        return 'rev-manifest.json';
+    }
+
+    /**
+     * @return string
+     */
+    protected function getManifestFileName()
+    {
+        // get location of our gulp-rev manifest file
+        $file = isset($this->config['rev_file']) ? $this->config['rev_file'] : $this->getDefaultFilename();
+        $filePath = $this->app['paths']['themepath'] . '/' . $file;
+        return $filePath;
+    }
+}
